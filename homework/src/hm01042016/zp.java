ab07855e399532db6311bc531ff0aa08787ea629
andronovdo <hitn89@gmail.com>
2016-04-01 09:16:51 +0300

    vichet naloga

---
+++ homework/src/hm01042016/zp.java
@@ -0,0 +1,16 @@
+package hm01042016;
+
+public class zp {
+
+	public static void main(String[] args) {
+		
+		int zp = 40000;
+		double nalog = 0.87;
+		double naruki = zp * nalog;
+	
+		System.out.print("За вычетом налогов остается: ");
+		System.out.print(naruki);
+		System.out.print(" руб");
+	}
+	
+}
