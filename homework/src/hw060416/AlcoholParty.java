a977dce4c963390ba384384076222fb3c2e5a12b
andronovdo <hitn89@gmail.com>
2016-04-06 09:44:46 +0300

    translate

--- homework/src/hw060416/AlcoholParty.java
+++ homework/src/hw060416/AlcoholParty.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class AlcoholParty {
 
 	public static void main(String[] args) {
-
+		
 		if (args.length == 0) {
 			System.out.println("Usage:  EvenOddExample <number>");
 			System.exit(0);
@@ -17,17 +17,17 @@ public class AlcoholParty {
 		int x = Integer.parseInt(args[0]);
 
 		if (x == 2) {
-			System.out.println("Много!!");
-		}
+			System.out.println("Много!!  a lot of");
+		} 
 		if (x == 5) {
-			System.out.println("Мало!!!");
-		}
+			System.out.println("Мало!!!  few");
+		} 
 		if (x == 1) {
-			System.out.println("В самый раз.");
-		}
+			System.out.println("В самый раз.  Just right.");
+		} 
 		if (x != 2 && x != 5 && x != 1) {
 			System.out.println("Fatal ERROR!");
-		}
+		} 
 	}
-
+	
 }
