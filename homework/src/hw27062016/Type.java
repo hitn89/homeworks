157fd34cbc471082983a412a34e7068dc3af8197
hitn <hitn89@gmail.com>
2016-06-27 22:35:17 +0300

    Type

---
+++ homework/src/hw27062016/Type.java
@@ -0,0 +1,32 @@
+package hw27062016;
+
+public class Type {
+
+	public static void main(String[] args) {
+
+		byte b = 7;
+		System.out.println("byte = " + b);
+		short s = b;
+		System.out.println("short = " + s);
+		int i = s;
+		System.out.println("int = " + i);
+		char c = '1';
+		System.out.println("char = " + c);
+		int i2 = c;
+		System.out.println("int2 = " + i2);
+		float f = i2;
+		System.out.println("float = " + f);
+		long l = i2;
+		System.out.println("long = " + l);
+		double d = l;
+		System.out.println("double = " + d);
+		double d2 = f;
+		System.out.println("double2 = " + d2);
+		double d3 = i2;
+		System.out.println("double3 = " + d3);
+		float f2 = l;
+		System.out.println("float2 = " + f2);
+
+	}
+
+}
